e83396f87a8c52e5439c03380a64191c225d43c7
Moustafa_Khedr <mustfakhedr1998@gmail.com>
2023-01-29 20:17:27 +0200

    after add serializer and Generic views

--- job/serializers.py
+++ job/serializers.py
@@ -1,1 +1,12 @@
 ## get data from models ---> json
+from rest_framework import serializers
+from .models import Job
+
+
+# Serializers define the API representation.
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
+
+
--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views            # . == path of the file in the same file of url
-
+from . import api
 
 app_name = 'job'
 
@@ -9,6 +9,12 @@ urlpatterns = [
     path('add', views.add_job, name='add_job'),
     path('<str:slug>', views.job_detail, name='job_detail'),    # ('<id>') بحدد له مسار واعرف في الرئيسي هلشان ميدخلش فى اللى فوق لما اطلبه والمسار بين الاقواس
                                                                 # id سابقا  (slug حاليا لعمل فلتر باسم الوظيفة)
+    # api
+    path('api/jobs', api.job_list_api, name='job_list_api'),
+    path('api/jobs/<int:id>', api.job_detail_api, name='job_detail_api'),
 
+    # Generic (class based views)
+    path('api/v2/jobs/', api.JobListApi.as_view(), name='JobListApi'),
+    path('api/v2/jobs/<int:id>', api.JobDetail.as_view(), name='JobDetail'),
 
 ]
